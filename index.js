0ec8c4ba4d509c98a54750bcdecb27075feec492
Tathagat017 <114250830+Tathagat017@users.noreply.github.com>
2023-05-13 10:32:56 +0530

    CORS POLICY CHANGES MADE IN MIDDLEWARE

--- index.js
+++ index.js
@@ -29,7 +29,17 @@ app.use(
     origin: "*",
   })
 );
+app.options("*", cors());
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept"
+  );
+  next();
+});
 app.use(express.json());
 
 //FOOD ROUTERS
